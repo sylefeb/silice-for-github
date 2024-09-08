d72c41b4999710c72c8a820ecf44a741415cfb3b
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2024-09-08 11:51:45 +0200

    initial attempt

---
+++ package.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir release
+mkdir release/bin
+INSTALL_DIR=/use/local/share
+cp /usr/local/bin/silice release/bin
+cp /usr/local/bin/silice-make.py release/bin
+cp /usr/local/bin/liblibsilice.a release/bin
+cp -R /usr/local/share/silice release/share/
+tar cvfz release release.tgz
